5c21099fc6bcb9b7993babd6d61646d671949378
rolwyn <rolwyn.quadras051@gmail.com>
2025-02-28 21:31:59 +0530

    Comment section added, skeleton and final styping remaining

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -11,6 +11,11 @@ const getBlogDatails = async (id: string) => {
   return response.json();
 }
 
+const getComments = async (id: string) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+  return response.json();
+}
+
 export default async function BlogPage({
 	params,
 }: {
@@ -19,13 +24,35 @@ export default async function BlogPage({
 ) {
 	const id = (await params).id;
 	const blogDetails = getBlogDatails(id);
-	const [blogData] = await Promise.all([blogDetails])
+	const commentDetails = getComments(id);
+	const [blog, comments] = await Promise.all([blogDetails, commentDetails])
 	return (
-		<article className="max-w-3xl mx-auto py-6">
-			<div className="space-y-4">
-				<h1 className="text-3xl font-bold">{blogData.title}</h1>
-				<p className="text-md">{blogData.body}</p>
-			</div>
-		</article>
+		<>
+			<article className="max-w-3xl mx-auto py-6 px-6 md:px-0">
+				<div className="space-y-4">
+					<h1 className="text-3xl font-bold capitalize text-sky-950">{blog.title}</h1>
+					<p className="text-md">{blog.body}</p>
+				</div>
+			</article>
+
+			<section className="max-w-3xl mx-auto py-6 px-6 md:px-0">
+				<h3 className="text-xl font-semibold mb-4 text-sky-950">Comments <span>({comments.length})</span></h3>
+				<div className="space-y-4">
+					{
+						comments.map((comment: Comment) => (
+							<div
+								key={comment.id}
+								className="flex flex-row"
+							>
+								<div className="flex mb-2 flex-col">
+									<div className="text-md text-sky-600">{comment.name}</div>
+									<div className="text-sm">{comment.body}</div>
+								</div>
+							</div>
+						))
+					}
+				</div>
+			</section>
+		</>
 	)
 }
--- components/blog-list.tsx
+++ components/blog-list.tsx
@@ -11,10 +11,10 @@ interface Post {
 export function BlogList({blog_posts}: {blog_posts: Post[]}) {
 	return (
 		<div className="grid gap-6 lg:grid-cols-3 md:grid-cols-2 px-10 py-4">
-			{blog_posts.map((blog) => (
+			{blog_posts.map((blog: Post) => (
 				<article key={blog.id} className="rounded-lg border p-6 m-2 mx-2">
 					<Link href={`/blogs/${blog.id}`} className="block space-y-3">
-						<h2 className="text-xl font-semibold underline">{blog.title}</h2>
+						<h2 className="text-xl font-semibold text-sky-950">{blog.title}</h2>
 						<p className="line-clamp-2">{blog.body}</p>
 						<div className="text-sm">Read more...</div>
 					</Link>
